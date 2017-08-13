849b9ba80b25789ca95a4199f85f865532b4a557
Alberto Jaime Le Baron <jimmleb178@gmail.com>
2017-08-13 17:15:58 -0600

    First personal website commit

---
+++ js/index.js
@@ -0,0 +1,23 @@
+$(document).ready(function() {
+  var scroll_pos = 0;
+  console.log(screen.width);
+  if (/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent) || screen.width < 1360) {
+    $(".logo").hide();
+    $(".icons").hide();
+    $(".mobile").show();
+  } else {
+    $(document).scroll(function() {
+      $(".text").html("_A");
+      $("#boxer").addClass("left");
+      $("#icon-animation").addClass("right");
+      $(".1").addClass("height");
+      scroll_pos = $(this).scrollTop();
+      if (scroll_pos === 0) {
+        $(".text").html("_Alberto " + "<span class='my-career'>front-end developer</span>");
+        $("#boxer").removeClass("left");
+        $("#icon-animation").removeClass("right");
+        $(".1").removeClass("height");
+      }
+    });
+  }
+});
