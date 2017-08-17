3e7b838a113d1a9495b63864904d7db53c2e6adf
Alberto Jaime Le Baron <jimmleb178@gmail.com>
2017-08-17 14:36:31 -0600

    Added to the photo panel (A slideshow)

--- js/index.js
+++ js/index.js
@@ -21,3 +21,25 @@ $(document).ready(function() {
     });
   }
 });
+
+var moveIndex = 1;
+pic(moveIndex);
+
+function move(n) {
+    pic(moveIndex += n);
+}
+
+function pic(n) {
+    var i;
+    var x = document.getElementsByClassName("slide");
+    if (n > x.length) {
+      moveIndex = 1;
+    }
+    if (n < 1) {
+      moveIndex = x.length;
+    }
+    for (i = 0; i < x.length; i++) {
+        x[i].style.display = "none";
+    }
+    x[moveIndex-1].style.display = "block";
+}
