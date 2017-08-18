83ad626aab778e477cebc8780ceebdf1eaf6103e
Alberto Jaime Le Baron <jimmleb178@gmail.com>
2017-08-17 21:31:31 -0600

    Added more content to the works panel

--- js/index.js
+++ js/index.js
@@ -1,27 +1,3 @@
-$(document).ready(function() {
-  var scroll_pos = 0;
-  console.log(screen.width);
-  if (/Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent) || screen.width < 1360) {
-    $(".logo").hide();
-    $(".icons").hide();
-    $(".mobile").show();
-  } else {
-    $(document).scroll(function() {
-      $(".text").html("_J");
-      $("#boxer").addClass("left");
-      $("#icon-animation").addClass("right");
-      $(".1").addClass("height");
-      scroll_pos = $(this).scrollTop();
-      if (scroll_pos === 0) {
-        $(".text").html("_Jaime " + "<span class='my-career'>front-end developer</span>");
-        $("#boxer").removeClass("left");
-        $("#icon-animation").removeClass("right");
-        $(".1").removeClass("height");
-      }
-    });
-  }
-});
-
 var moveIndex = 1;
 pic(moveIndex);
 
