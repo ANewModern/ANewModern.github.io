7e76034cc8dd59538e98d0c4af2a0577affe3255
ANewModern <jimmleb178@gmail.com>
2017-09-02 08:43:41 -0600

    Remade the logo to appear on scroll almost perfectly, redid the logo code for html and simplified the jquery

--- js/index.js
+++ js/index.js
@@ -2,44 +2,49 @@ var moveIndex = 1;
 pic(moveIndex);
 
 function move(n) {
-    pic(moveIndex += n);
+  pic(moveIndex += n);
 }
 
 function pic(n) {
-    var i;
-    var x = document.getElementsByClassName("slide");
-    if (n > x.length) {
-      moveIndex = 1;
-    }
-    if (n < 1) {
-      moveIndex = x.length;
-    }
-    for (i = 0; i < x.length; i++) {
-        x[i].style.display = "none";
-    }
-    x[moveIndex-1].style.display = "block";
+  var i;
+  var x = document.getElementsByClassName("slide");
+  if (n > x.length) {
+    moveIndex = 1;
+  }
+  if (n < 1) {
+    moveIndex = x.length;
+  }
+  for (i = 0; i < x.length; i++) {
+    x[i].style.display = "none";
+  }
+  x[moveIndex - 1].style.display = "block";
 }
 
 $(document).ready(function() {
+  $("a[href='#top']").click(function() {
+    $("html, body").animate({
+      scrollTop: 0
+    }, "slow");
+    return false;
+  });
   var scroll_pos = 0;
   $(document).scroll(function() {
-    $(".immy").css("display", "none");
     $(".line").addClass("color");
-    $(".name-container").addClass("not-mid");
     $(".top-line").removeClass("right");
     $(".left-line").removeClass("up");
     $(".right-line").removeClass("bot");
     $(".bot-line").removeClass("left");
-
+    $(".name-container").addClass("not-mid");
+    $(".immy").css("visibility", "hidden");
     scroll_pos = $(this).scrollTop();
-    if (scroll_pos === 0) {
-      $(".immy").css("display", "inline");
+    if (scroll_pos <= 100) {
       $(".line").removeClass("color");
-      $(".name-container").removeClass("not-mid");
       $(".top-line").addClass("right");
       $(".left-line").addClass("up");
       $(".right-line").addClass("bot");
       $(".bot-line").addClass("left");
+      $(".name-container").removeClass("not-mid");
+      $(".immy").css("visibility", "visible");
     }
   });
 });
