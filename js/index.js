1351298ac657c7d2942381ce4128d0b4a8a8fd3f
Alberto Jaime Le Baron <jimmleb178@gmail.com>
2018-05-14 12:23:17 -0500

    Made complete revamp to the website

--- js/index.js
+++ js/index.js
@@ -1,50 +1,9 @@
-var moveIndex = 1;
-pic(moveIndex);
+$('.project').click(function(){
+  $('.project').css('height', '100px');
+  $(this).css('height', '300px'); 
+});
 
-function move(n) {
-  pic(moveIndex += n);
-}
-
-function pic(n) {
-  var i;
-  var x = document.getElementsByClassName("slide");
-  if (n > x.length) {
-    moveIndex = 1;
-  }
-  if (n < 1) {
-    moveIndex = x.length;
-  }
-  for (i = 0; i < x.length; i++) {
-    x[i].style.display = "none";
-  }
-  x[moveIndex - 1].style.display = "block";
-}
-
-$(document).ready(function() {
-  $("a[href='#top']").click(function() {
-    $("html, body").animate({
-      scrollTop: 0
-    }, "slow");
-    return false;
-  });
-  var scroll_pos = 0;
-  $(document).scroll(function() {
-    $(".line").addClass("color");
-    $(".top-line").removeClass("right");
-    $(".left-line").removeClass("up");
-    $(".right-line").removeClass("bot");
-    $(".bot-line").removeClass("left");
-    $(".name-container").addClass("not-mid");
-    $(".immy").css("visibility", "hidden");
-    scroll_pos = $(this).scrollTop();
-    if (scroll_pos <= 100) {
-      $(".line").removeClass("color");
-      $(".top-line").addClass("right");
-      $(".left-line").addClass("up");
-      $(".right-line").addClass("bot");
-      $(".bot-line").addClass("left");
-      $(".name-container").removeClass("not-mid");
-      $(".immy").css("visibility", "visible");
-    }
-  });
+$('.project-large').click(function(){  
+  $('.project-large').css('width', '5%');
+  $(this).css('width', '90%'); 
 });
